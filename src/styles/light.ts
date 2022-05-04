eae980a8151715305b56ecb9f1ef34fd1c73f440
Ivan Lucas de Oliveira Ribeiro <ivanoliver131@gmail.com>
2022-05-03 23:40:02 -0300

    devCommit

--- src/styles/light.ts
+++ src/styles/light.ts
@@ -4,16 +4,19 @@ import { extendTheme } from '@chakra-ui/react';
 export const light = extendTheme({
   colors: {
     gray: {
-      "900": "#C71585",
-      "800": "#FF1493",
-      "700": "#FF69B4",
-      "600": "#DB7093",
-      "500": "#FFB6C1",
-      "400": "#F08080",
-      "300": "#F08080",
-      "200": "#B3B5C6",
-      "100": "#CD5C5C",
-      "50": "#DC143C",
+       "900": "#4B0082",
+       "800": "#E6E6FA ",
+       "700": "#DDA0DD",
+       "600": "#EE82EE",
+       "500": "#DA70D6",
+       "400": "#FF00FF",
+       "300": "#BA55D3",
+       "200": "#9370DB",
+       "100": "#8A2BE2",
+       "50": "#9400D3",
+    },
+    white: {
+      "900": "#FFFFFF"
     }
   },
   fonts: {
@@ -24,7 +27,7 @@ export const light = extendTheme({
     global: {
       body: {
         bg: 'white.900',
-        color: 'purple.50'
+        color: 'purple.300'
       }
     }
   }
