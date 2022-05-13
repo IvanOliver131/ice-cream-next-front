a426e5885ae85825b52f681438d9bb41593c4861
Ivan Lucas de Oliveira Ribeiro <ivanoliver131@gmail.com>
2022-05-12 23:38:21 -0300

    devCommit

--- src/hooks/StyleHook.ts
+++ src/hooks/StyleHook.ts
@@ -1,7 +1,7 @@
 import { useDarkmodeContext } from "../contexts/DarkmodeContext";
 import { dark, light } from "../styles";
 
-// Um hook para utilizar o darkmode
+// * Um hook para utilizar o darkmode
 export function StyleHook () {
   const { mode } = useDarkmodeContext();
   const theme = mode === 'dark' ? dark : light;
